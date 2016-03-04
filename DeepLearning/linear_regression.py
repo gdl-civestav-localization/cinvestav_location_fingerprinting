695e8eb76e0bba9188c97bd4468bd6cd605132f6
Gibran Felix <gibranfelix@gmail.com>
2016-03-03 19:50:51 -0600

    Se modifico el modelo de regrecion linear

--- DeepLearning/linear_regression.py
+++ DeepLearning/linear_regression.py
@@ -1,57 +1,58 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
+import scipy
 import theano
-from theano import function
-from theano import pp
-from theano import tensor as T
+import theano.tensor as T
+import numpy
 
 # Load training data.
-train_x = np.asarray([[0.0], [1.0], [2.0], [3.0], [4.0]], dtype=theano.config.floatX)
-train_y = np.asarray([1.0, 1.1, 3.0, 2.0, 5.0], dtype=theano.config.floatX)
+train_x = numpy.array([[1, 9], [2, 5], [1.5, 3], [3, 7]], dtype=theano.config.floatX)
+train_y = numpy.array([[2, 5], [4, 5], [3, 7], [0, 3]], dtype=theano.config.floatX)
 
 # Parameters.
-learning_rate = 0.1
-n_dims = 1
-n_steps = 1000
+learning_rate = .001
+n_steps = 10000
+in_dims = 2
+out_dims = 2
 
 # Define model.
-x = T.matrix(name='x')  # Input matrix with examples.
-w = theano.shared(
-    value=np.zeros(
-        (n_dims, 1),  # Parameters of the model.
-        dtype=theano.config.floatX),
-    name='w',
-    borrow=True)
+W = theano.shared(
+        value=numpy.random.rand(in_dims, out_dims).astype(dtype=theano.config.floatX),
+        name='w',
+        borrow=True)
+
+x = T.matrix(name='x', dtype=theano.config.floatX)
+z = T.matrix(name='z', dtype=theano.config.floatX)
+b = theano.shared(
+        value=numpy.random.rand(out_dims, ).astype(dtype=theano.config.floatX),
+        name='b',
+        borrow=True
+    )
 
 # Define objective function.
-f = function([x], T.dot(w, x))  # Linear regression.
-
-y = T.vector(name='y')  # Output vector with y values.
+y = T.dot(x, W) + b  # Linear regression.
+prediction = theano.function(inputs=[x], outputs=y)  # Linear regression.
 
 # Define loss function.
-loss = T.mean((T.dot(x, w).T - y) ** 2)
+loss = T.mean((z - y) ** 2)
 
 # Build the gradient descent algorithm.
-g_loss = T.grad(loss, wrt=w)
+g_W = T.grad(cost=loss, wrt=W)
+g_b = T.grad(cost=loss, wrt=b)
 
-train_model = function(inputs=[],
-                       outputs=loss,
-                       updates=[(w, w - learning_rate * g_loss)],
-                       givens={
-                           x: train_x,
-                           y: train_y
-                       })
+train_model = theano.function(
+    inputs=[x, z],
+    outputs=loss,
+    updates=[
+        (W, W - learning_rate * g_W),
+        (b, b - learning_rate * g_b)
+    ])
 
-# Execute the gradient descent algorithm.
+# Run stupid gradient descent.
 for i in range(n_steps):
-    print "cost", train_model()
+    error = train_model(train_x, train_y)
+    print "cost", error
 
-# Plot results.
-plt.figure()
-plt.plot([a[0] for a in train_x], train_y, 'r')
-plt.plot([a[0] for a in train_x], [f([xx])[0] for xx in train_x], 'b')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('title')
-plt.show()
+print W.get_value()
+print b.get_value()
+print "finis"
+
+print prediction(train_x)

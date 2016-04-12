51f09a0cf3c251b99a286bdb0c367d576f09a69c
Gibran Felix <gibranfelix@gmail.com>
2016-04-12 14:35:22 -0500

    Experiments was moved to __init__

---
+++ preprocessing/__init__.py
@@ -0,0 +1,63 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from datasets import DatasetManager
+from preprocessing.dimensionality_reduction import get_pca
+from preprocessing.scaling import get_gaussian_normalization, get_linear_normalization
+
+
+def run_experiment():
+    train_set, valid_set, test_set = DatasetManager.read_dataset()
+    dataset, result = train_set
+
+    # Reduce to a 2D dimensionality for plotting the data
+    dataset = get_gaussian_normalization(dataset)
+    # dataset = get_LLE(dataset, num_components=2, n_neighbors=80)
+    dataset, explained_variance_ratio_ = get_pca(dataset, num_components=2)
+
+    plot_embedding(dataset, result)
+
+
+def plot_embedding(dataset, result):
+    """
+    Plot clusters with same labels and label as legends
+    Parameters
+    ----------
+    dataset: Values to plot
+    result: Labels of the data
+    """
+    dataset = np.asarray(dataset)
+    result = np.asarray(result)
+
+    dataset = get_linear_normalization(dataset)
+
+    used_labels = []
+    ax = plt.subplot(111)
+    shown_images = [[1., 1.]]
+    for c in xrange(len(result)):
+        if result[c].tolist() not in used_labels:
+            used_labels.append(result[c].tolist())
+            cluster = []
+            for i in xrange(len(dataset)):
+                if np.array_equal(result[i], result[c]):
+                    cluster.append(dataset[i])
+            plt.plot(*zip(*cluster), marker='o')
+
+            dist = np.sum((cluster[0] - shown_images) ** 2, 1)
+            if np.min(dist) < 1e-3:
+                continue
+
+            from matplotlib.offsetbox import TextArea, AnnotationBbox
+            text = '(' + str(int(result[c][0])) + ',' + str(int(result[c][1])) + ')'
+            text_area = TextArea(text, minimumdescent=False)
+            ab = AnnotationBbox(text_area, cluster[0],
+                                xybox=(-20, 40),
+                                xycoords='data',
+                                boxcoords="offset points",
+                                arrowprops=dict(arrowstyle="->"))
+            ax.add_artist(ab)
+            shown_images.append(cluster[0])
+    plt.show()
+
+
+if __name__ == '__main__':
+    run_experiment()
--- preprocessing/manifold.py
+++ preprocessing/manifold.py
@@ -1,9 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
-
-from datasets import DatasetManager
-from preprocessing.dimensionality_reduction import get_pca
-from preprocessing.scaling import get_gaussian_normalization, get_linear_normalization
 
 
 # Perform a LLE transformation
@@ -22,44 +17,3 @@ def get_TSNE(data=np.array([]), num_components=2):
 
     x_lle = clf.fit_transform(data)
     return x_lle
-
-
-def run_experiment():
-    dataset, result = DatasetManager.read_dataset()
-
-    # Reduce to a 2D dimensionality for plotting the data
-    dataset = get_gaussian_normalization(dataset)
-    # dataset = get_LLE(dataset, num_components=2, n_neighbors=80)
-    dataset, explained_variance_ratio_ = get_pca(dataset, num_components=2)
-
-    plot_embedding(dataset, result)
-
-
-def plot_embedding(dataset, result):
-    dataset = get_linear_normalization(dataset)
-    used_labels = []
-    ax = plt.subplot(111)
-    shown_images = [[1., 1.]]
-    for c in xrange(len(result)):
-        if result[c] not in used_labels:
-            used_labels.append(result[c])
-            cluster = []
-            for i in xrange(len(dataset)):
-                if result[i] == result[c]:
-                    cluster.append(dataset[i])
-            plt.plot(*zip(*cluster), marker='o')
-
-            dist = np.sum((cluster[0] - shown_images) ** 2, 1)
-            if np.min(dist) < 1e-3:
-                continue
-            from matplotlib.offsetbox import TextArea, AnnotationBbox
-            text = '(' + str(int(result[c][0])) + ',' + str(int(result[c][1])) + ')'
-            text_area = TextArea(text, minimumdescent=False)
-            ab = AnnotationBbox(text_area, cluster[0],
-                                xybox=(-20, 40),
-                                xycoords='data',
-                                boxcoords="offset points",
-                                arrowprops=dict(arrowstyle="->"))
-            ax.add_artist(ab)
-            shown_images.append(cluster[0])
-    plt.show()

525096557b11e3a36c02ab2a155e73621d17901b
Gibran Felix <gibranfelix@gmail.com>
2016-04-12 18:08:30 -0500

    Some files was deleted

--- cluster/__init__.py
+++
@@ -1,8 +0,0 @@
-from preprocessing.manifold import run_experiment
-
-if __name__ == '__main__':
-    run_experiment()
-
-    # result, dataset = DatasetManager.read_dataset()
-    # dataset = get_gaussian_normalization(dataset)
-    # print dataset

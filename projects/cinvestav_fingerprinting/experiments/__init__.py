12c7310d6f9dbdffeeccde90adbf7c9005089826
unknown <gibranfelix>
2016-10-26 10:45:33 -0500

    Added experiment

--- projects/cinvestav_fingerprinting/experiments/__init__.py
+++ projects/cinvestav_fingerprinting/experiments/__init__.py
@@ -1,14 +1,7 @@
-import os
 import cPickle
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-
-pd.set_option('expand_frame_repr', False)
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-show_results = True
+import glob
+from projects.cinvestav_fingerprinting.experiments.utils import get_metrics
 
 
 def plot_cost(results, data_name, plot_label):
@@ -25,57 +18,52 @@ def plot_cost(results, data_name, plot_label):
     plt.legend(fontsize='xx-large')
 
 
-def get_metrics(results):
-    lst_error = []
-    metrics = []
-    for i in results:
-        model_name = i['Name']
-        predicted_values = i['predicted_values']
-        test_set = i['test_set']
+def plot_per_experiment():
+    for filename in glob.glob('*all_model*'):
+        with open(filename, 'rb') as f:
+            results = cPickle.load(f)
+        results.pop(0)  # Remove params
 
-        test_error = np.linalg.norm(test_set - predicted_values, axis=1)
-        test_error = test_error.astype(dtype=np.float64)
+        dnn_relu_models = results[0:10]
+        dnn_sigmoid_models = results[10:20]
+        dnn_tanh_models = results[20:30]
+        gdbn_models = results[30:40]
+        dbn_models = results[40:50]
 
-        min_value = test_error.min()
-        max_value = test_error.max()
-        avg_value = test_error.mean()
-        std_value = test_error.std()
-        test_error.sort()
+        plt.figure(filename)
+        plt.ylabel('Accuracy (m)', fontsize=30)
+        plt.xlabel('Layers', fontsize=30)
+        plt.grid(True)
+        plt.tick_params(axis='both', which='major', labelsize=20)
 
-        metrics.append([
-            model_name,
-            min_value,
-            max_value,
-            avg_value,
-            std_value,
-            test_error[int(len(test_error) * .95)],
-            test_error[int(len(test_error) * .66)],
-            test_error[int(len(test_error) * .50)]
-        ])
-        lst_error.append(avg_value)
-        # print_metrics(euclidean_error)
+        plt.plot(get_metrics(results=dnn_relu_models), label='DNN Relu', linewidth=5.0)
+        plt.plot(get_metrics(results=dnn_sigmoid_models), label='DNN Sigmoid', linewidth=5.0)
+        plt.plot(get_metrics(results=dnn_tanh_models), label='DNN Tanh', linewidth=5.0)
+        plt.plot(get_metrics(results=gdbn_models), label='GR-DBN', linewidth=5.0)
+        plt.plot(get_metrics(results=dbn_models), label='DBN', linewidth=5.0)
+        plt.legend(fontsize='large')
+    plt.show()
 
-    columns = [
-        '',
-        'Min',
-        'Max',
-        'Mean',
-        'Std',
-        '95',
-        '66',
-        '50'
-    ]
-    dataframe = pd.DataFrame(
-        data=metrics,
-        columns=columns
-    )
-    dataframe = dataframe.T
-    dataframe.columns = dataframe.iloc[0]
-    dataframe = dataframe[1:]
-    if show_results:
-        print dataframe.head(10)
 
-    return lst_error
+def plot_all_experiments():
+    plt.figure("Depth experiement")
+    plt.ylabel('Accuracy (m)', fontsize=30)
+    plt.xlabel('Layers', fontsize=30)
+    plt.ylim([0, 10])
+    plt.yticks(range(0, 11))
+    plt.grid(True)
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    for filename in glob.glob('*seed*'):
+        with open(filename, 'rb') as f:
+            results = cPickle.load(f)
+        results.pop(0)  # Remove params
+
+        lst_error = get_metrics(
+            results=results
+        )
+        plt.plot(lst_error, label=filename, linewidth=5.0)
+    plt.legend(fontsize='large')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -98,21 +86,6 @@ if __name__ == '__main__':
         plot_label='Cost on test phase')
     plt.show()
     """
+    # plot_all_experiments()
 
-    with open(os.path.join(os.path.dirname(__file__), 'all_models_with_out_noise_neither_dropout_theano_seed_5'), 'rb') as f:
-        results = cPickle.load(f)
-    results.pop(0)  # Remove params
-
-    lst_error = get_metrics(
-        results=results
-    )
-
-    plt.figure("Depth experiement")
-    plt.ylabel('Accuracy (m)', fontsize=30)
-    plt.xlabel('Layers', fontsize=30)
-    # plt.yscale('symlog')
-    plt.grid(True)
-    plt.tick_params(axis='both', which='major', labelsize=20)
-    plt.plot(lst_error, linewidth=5.0)
-    plt.show()
-    print lst_error
+    plot_per_experiment()
